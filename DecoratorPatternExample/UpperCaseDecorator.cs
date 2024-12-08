7273e81c91710176c8772548a8b4df904ad1cfda
manos <manos.akpujiha@gmail.com>
2024-12-08 02:15:20 -0500

    Add decorator classes for text formatting: ColorDecorator, PlainDecorator, and UpperCaseDecorator

---
+++ DecoratorPatternExample/UpperCaseDecorator.cs
@@ -0,0 +1,14 @@
+namespace DecoratorPatternExample
+{
+    public class UpperCaseDecorator : Decorator<string>
+    {
+        public UpperCaseDecorator(IComponent<string> component) : base(component)
+        {
+        }
+
+        public override string GetText()
+        {
+            return base.GetText().ToUpper();
+        }
+    }
+}
